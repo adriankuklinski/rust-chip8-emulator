d1e45157d3c8e7c0f9ec7af5b5877f0a8e24cc16
Adrian Kuklinski <adrian.kuklinski@eventconnect.io>
2023-04-16 08:52:17 -0400

    feat: read test binary file and store in Vec<u8>

---
+++ src/main.rs
@@ -0,0 +1,8 @@
+use std::fs::File;
+use std::io::Read;
+
+fn main() {
+    let mut file = File::open("data/INVADERS").unwrap();
+    let mut data = Vec::<u8>::new();
+    file.read_to_end(&mut data);
+}
